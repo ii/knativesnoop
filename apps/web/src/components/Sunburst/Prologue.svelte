5d7ce3eff0ca40f0dafe018bf6da46b6680945ad
zachmandeville <webmaster@coolguy.website>
2023-10-06 09:26:34 +1300

    adjust prologue in sunburst

--- apps/web/src/components/Sunburst/Prologue.svelte
+++ apps/web/src/components/Sunburst/Prologue.svelte
@@ -24,8 +24,8 @@
 
 {#if release}
   <SectionHeader title={""}>
-    <h2>{release} Testing Coverage
-      <button on:click={() => releaseSwitch = true}>switch release</button>
+    <h2>{release} Knative Testing Coverage
+      <!-- <button on:click={() => releaseSwitch = true}>switch release</button> -->
     </h2>
     {#if releaseSwitch}
       <ul class='releases'>
@@ -34,7 +34,7 @@
       {/each}
       </ul>
     {:else}
-      <em>Data comes from 1 e2e test suite run from {date}:</em>
+      <!-- <em>Data comes from 1 e2e test suite run from {date}:</em> -->
       <!-- <ul class='sources'>
            {#each sources as source}
            <li>
@@ -45,13 +45,28 @@
            {/each}
            </ul> -->
     {/if}
-    <p>This sunburst shows the testing coverage for the Knative API CRD's on a Kubernetes cluster.
-      It generated this api by deploying knative serving/eventing to a kind cluster, then pulling
-      the dynamic open api spec from within the cluster.  The testing is at 0% until a consistently
-      accurate method is found to grab Knative's test suite audit logs.  For Kubernetes, we were able
-      to pull audit logs from a conformance suite job run every few hours, but have not found a
-      satisfactory knative equivalent yet.
+    <p>
+      The Kubernetes conformance project allows vendors to publicly verify their offering is conformant to the k8s spec, through
+      running the suite via Sonobuoy and submitting it to the <a href="https://github.com/cncf/k8s-conformance/blob/master/instructions.md">k8s conformance repo</a>.</p>
+    <p>
+      APISnoop was implemented to track the progress of conformance coverage, running the tests with an audit sink to check each
+      k8s stable endpoint is covered by some test.</p>
+    <p>
+      This tool is used as part of the <a href="https://testgrid.k8s.io/sig-arch-conformance#apisnoop-conformance-gate">release cycle of K8s</a>, to ensure no new GA endpoints are added without coverage. It is also
+      used by test writers to help with the focus and accuracy of their tests.
     </p>
+    <p>
+      We are interested if this project can be extended to help Knative conformance. Our current questions are:
+    </p>
+    <ul>
+      <li>How does Knative currently measure coverage progress?  If one were to say "Knative is 100% covered by conformance tests", how would
+        that be shown or verified?</li>
+      <li>Where does the knative spec live? Does it consist of Kubernetes CRD's only? If not, what else is included in the spec?</li>
+      <li>Is there a recommended method and target for running the Knative suite currently?</li>
+      <li>The suite is run as a gh action using KinD. Is there a specific kind config that should be used if wnating to reproduce the action on our own KinD cluster?</li>
+
+    </ul>
+
     <p>
     The endpoints are organized by
     level (alpha, beta, or stable), then category. The color of an endpoint
